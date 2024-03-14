f8a0c60fe5343b71d6718f48dfaf7fbbbbb9ac99
Nicolas Grévin <developer@nicolas-grevin.com>
2024-03-14 16:53:53 +0100

    fix(javascript) Fix astronaut hydration

--- codes/javascript/src/repository/astronautRepository.js
+++ codes/javascript/src/repository/astronautRepository.js
@@ -2,18 +2,24 @@ import Astronaut from '../model/astronaut.js'
 
 import postgresClient from '../client/postgresClient.js'
 
-const hydrate = data => {
+const hydrate = (data) => {
   if (Array.isArray(data)) {
-    for (const row of data) {
-      hydrate(row)
-    }
+    return data.map(r => hydrate(r))
   }
 
   return new Astronaut(data.id, data.name)
 }
 
-const findAll = async () => hydrate(await postgresClient.query('SELECT id, name FROM astronauts OFFSET 0 LIMIT 100').rows)
+const findAll = async () => {
+  const data = await postgresClient.query('SELECT id, name FROM astronauts OFFSET 0 LIMIT 100')
 
-const findById = async id => hydrate(await postgresClient.query('SELECT id, name FROM astronauts WHERE id = $1', [id]).rows[0])
+  return hydrate(data.rows)
+}
+
+const findById = async (id) => {
+  const data = await postgresClient.query('SELECT id, name FROM astronauts WHERE id = $1', [id])
+
+  return hydrate(data.rows[0])
+}
 
 export { findAll, findById }
